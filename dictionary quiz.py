c90a34fa726b1ed877d47a68a72f5ec37cb6aa0f
CaseyGann <cmoden@gannacademy.org>
2023-09-14 10:10:09 -0400

    init

---
+++ dictionary quiz.py
@@ -0,0 +1,11 @@
+def grouping(L1, L2):
+    pass
+
+def main():
+    first_names = ["Ariel", "Adin", "Adam", "Jordan" ,"Joel", "Liat", "Aliya", "Jessica", "Jill", "Brandon",  "Alex", "Sarah",  "Amitai"]
+    last_names = ["Skolnick", "Wanderer", "Taglieri", "Kramer","Lederman", "Skolnick", "Taglieri", "Lederman","Kramer",  "Kramer", "Taglieri","Skolnick", "Wanderer"]
+
+    grouping(first_names, last_names)
+
+if __name__ == "__main__":
+    main()
