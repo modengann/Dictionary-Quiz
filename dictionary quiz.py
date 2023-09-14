26b6ea13138e7ff4758316cf3aca168ccbe9a4f0
CaseyGann <cmoden@gannacademy.org>
2023-09-14 10:13:33 -0400

    update add print

--- dictionary quiz.py
+++ dictionary quiz.py
@@ -5,7 +5,7 @@ def main():
     first_names = ["Ariel", "Adin", "Adam", "Jordan" ,"Joel", "Liat", "Aliya", "Jessica", "Jill", "Brandon",  "Alex", "Sarah",  "Amitai"]
     last_names = ["Skolnick", "Wanderer", "Taglieri", "Kramer","Lederman", "Skolnick", "Taglieri", "Lederman","Kramer",  "Kramer", "Taglieri","Skolnick", "Wanderer"]
 
-    grouping(first_names, last_names)
+    print(grouping(first_names, last_names))
 
 if __name__ == "__main__":
     main()
